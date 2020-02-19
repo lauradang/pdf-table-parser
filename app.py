4f1101be082d4683df9b168792c543f27f706678
Laura Dang <lauradang.2000@gmail.com>
2020-02-18 21:15:29 -0500

    Implement image editor html

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+#!/usr/bin/python3
 from flask import Flask, request, render_template, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 import os
 from pdf_to_jpg import pdf_to_jpg, resize_image
 from crop import crop
+import cv2
 
-UPLOAD_FOLDER = "pdfs"
-VISUALIZE_FOLDER = "visualize"
+UPLOAD_FOLDER = "pdf"
 IMAGE_FOLDER = "jpg"
+VISUALIZE_FOLDER = os.path.join("static", "images")
 ALLOWED_EXTENSIONS = {"pdf"}
 
 app = Flask(__name__)
@@ -14,10 +16,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config["IMAGE_FOLDER"] = IMAGE_FOLDER
 app.config["VISUALIZE_FOLDER"] = VISUALIZE_FOLDER
 
-# for folder in app
-# dir = os.path.join("visualize")
-# if not os.path.exists(dir):
-#     os.mkdir(dir)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 def allowed_file(filename):
     return "." in filename and \
@@ -55,13 +54,39 @@ def edit_image():
         if not pdf_files:
             return redirect(url_for("upload_file"))
 
-        pdf_to_jpg(pdf_files[0])
+        pdf_to_jpg(os.path.join(app.config["UPLOAD_FOLDER"], pdf_files[0]))
 
         for jpg in jpg_files:
-            image = crop("jpg/{}".format(jpg), False)
-            image.save(os.path.join(app.config["VISUALIZE_FOLDER"], jpg))
+            image = crop(os.path.join(app.config["IMAGE_FOLDER"], jpg), False)
+            cv2.imwrite(os.path.join(app.config["VISUALIZE_FOLDER"], jpg), image)    
+    
+    elif request.method == "POST":
+        dilation = int(request.form["dilation"])
+        erosion = int(request.form["erosion"])
+        min_width = int(request.form["min_width"])
+        min_height = int(request.form["min_height"])
+        max_width = int(request.form["max_width"])
+        max_height = int(request.form["max_height"])
+        old_image = os.path.basename(request.form["image"])
 
+        print(old_image)
+        new_image = crop(
+            os.path.join(app.config["IMAGE_FOLDER"], old_image), 
+            False,
+            dilation=dilation,
+            erosion=erosion,
+            min_height=min_height,
+            min_width=min_width,
+            max_height=max_height,
+            max_width=max_width
+        )
+
+        print(request.form)
+        cv2.imwrite(os.path.join(app.config["VISUALIZE_FOLDER"], old_image), new_image)    
+   
+    visualize = [os.path.join(app.config["VISUALIZE_FOLDER"], file) for file in os.listdir(app.config["VISUALIZE_FOLDER"])]
+    print(visualize)
     return render_template(
         "edit_image.html",
-        visualize=os.listdir(app.config["VISUALIZE_FOLDER"])
+        visualize=visualize
     )
